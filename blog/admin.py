8093aa198504f8b8c09205e40e6ff8d4c7dd4771
MatinMat8 <matinmat8.mat@gmail.com>
2023-06-26 12:29:06 +0430

    adding post table on admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('auther', 'pk', 'title', 'draft_note', 'create', 'slug')
