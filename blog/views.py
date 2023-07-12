d79d45835cb80644357173b0f01693aa838f75dc
MatinMat8 <matinmat8.mat@gmail.com>
2023-07-12 12:32:50 +0430

    creating index view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import View
+from .models import Post
 
 # Create your views here.
+
+
+class Index(View):
+    def get(self, *args, **kwargs):
+        return render(self.request, 'blog/index.html')
