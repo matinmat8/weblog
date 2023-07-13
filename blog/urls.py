bb1d95048fe495e5076a3fec09a8f5a0bc4059de
MatinMat8 <matinmat8.mat@gmail.com>
2023-07-14 01:39:37 +0430

    adding list view and template

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import Index
-
+from .views import Index, PostLIst
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', Index.as_view(), name='index')
+    path('', Index.as_view(), name='index'),
+    path('list/', PostLIst.as_view(), name='list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, ListView
+# from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
+
 
 # Create your views here.
 
@@ -8,3 +10,12 @@ from .models import Post
 class Index(View):
     def get(self, *args, **kwargs):
         return render(self.request, 'blog/index.html')
+
+
+class PostLIst(ListView):
+    model = Post
+    template_name = 'blog/list.html'
+
+    def get_queryset(self):
+        obj = super().get_queryset()
+        return obj.filter().order_by('publish')
