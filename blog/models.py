3a92fb763249bed601334e1d2be07c6110e02849
MatinMat8 <matinmat8.mat@gmail.com>
2023-07-15 14:39:50 +0430

    adding model for saving views

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
 
@@ -18,3 +19,18 @@ class Post(models.Model):
     draft_note = models.CharField(max_length=250, blank=True)  # Note of why it is draft and when it is going to publish
     slug = models.CharField(max_length=150, unique_for_date='publish')  # for making unique urls
     status = models.CharField(max_length=60, choices=STATUS_CHOICES, default='draft')
+
+    @property
+    def views_count(self):
+        return PostView.objects.filter(post=self).count()
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'slug': self.slug})
+
+
+class PostView(models.Model):
+    IPAddress = models.GenericIPAddressField(default="45.243.82.169")
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{0} in {1} post'.format(self.IPAddress, self.post.title)
