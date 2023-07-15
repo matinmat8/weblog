393b84364d9da30d54d1931633910f2c16bf7eee
MatinMat8 <matinmat8.mat@gmail.com>
2023-07-15 14:40:51 +0430

    logic of views base on IPs

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Index, PostLIst
+from .views import Index, PostLIst, PostsDetailsView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('list/', PostLIst.as_view(), name='list'),
+    path('detail/<slug:slug>/', PostsDetailsView, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View, ListView
 # from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Post
+from .models import Post, PostView
 
 
 # Create your views here.
@@ -19,3 +19,14 @@ class PostLIst(ListView):
     def get_queryset(self):
         obj = super().get_queryset()
         return obj.filter().order_by('publish')
+
+
+def PostsDetailsView(request, slug):
+    post = Post.objects.get(slug=slug)
+    user_ip = request.META.get('HTTP_X_FORWARDED_FOR')
+    if user_ip:
+        ip = user_ip.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    PostView.objects.get_or_create(IPAddress=ip, post=post)
+    return render(request, 'blog/detail.html', {'post': post})
