a2b277fe8c9c141382948a439fbeaf57bf42f772
MatinMat8 <matinmat8.mat@gmail.com>
2023-07-20 13:57:10 +0430

    Customized exception handling middleware for practicing middlewares in Django

--- core/settings.py
+++ core/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'CustomMiddlewares.exception_middleware.ExceptionHandlingMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
