8daf3a7ebd247d269d6b028ce2cfbc4c58afc54b
MatinMat8 <matinmat8.mat@gmail.com>
2023-07-22 19:37:18 +0430

    customized debugging decorator for practicing decorators

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import View, ListView
-# from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, PostView
+
+from .decorators import debugging_decorator
+from django.utils.decorators import method_decorator
 
 
 # Create your views here.
@@ -10,6 +12,10 @@ from .models import Post, PostView
 class Index(View):
     def get(self, *args, **kwargs):
         return render(self.request, 'blog/index.html')
+
+    @method_decorator(debugging_decorator)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 
 class PostLIst(ListView):
@@ -20,7 +26,12 @@ class PostLIst(ListView):
         obj = super().get_queryset()
         return obj.filter().order_by('publish')
 
+    @method_decorator(debugging_decorator)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
+
+@debugging_decorator
 def PostsDetailsView(request, slug):
     post = Post.objects.get(slug=slug)
     user_ip = request.META.get('HTTP_X_FORWARDED_FOR')
